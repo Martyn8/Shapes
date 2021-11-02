dc409632de75a9c25709ee54c24d0eb0cc878d97
Martyna <martyn8mj@gmail.com>
2021-11-02 14:58:11 +0100

    feat: show figures, create adapter

---
+++ Shapes/app/src/main/java/com/example/shapes/Adapter.kt
@@ -0,0 +1,81 @@
+package com.example.shapes
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.ArrayList
+
+class Adapter(private val context: Context,
+                    private val dataSource: ArrayList<Figure>) : BaseAdapter() {
+
+    private val inflater: LayoutInflater
+            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    //1
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+    //2
+    override fun getItem(position: Int): Any {
+        return dataSource[position]
+    }
+
+    //3
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    //4
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        // Get view for row item
+        val rowView = inflater.inflate(R.layout.row, parent, false)
+
+
+// Get thumbnail element
+        val thumbnailImageView = rowView.findViewById(R.id.rowImage) as ImageView
+
+        // Get area element
+        val areaTextView = rowView.findViewById(R.id.rowArea) as TextView
+
+// Get detail element
+        val detailTextView = rowView.findViewById(R.id.rowDetail) as TextView
+
+
+        // 1
+        val figure = getItem(position) as Figure
+
+// 2
+
+        areaTextView.text = figure.figureArea.toString()
+        detailTextView.text = figure.characteristic
+
+// 3
+        /*Picasso.with(context).load(recipe.imageUrl).placeholder(R.mipmap.ic_launcher).into(thumbnailImageView)
+
+        ImageView image =
+        image.setImageResource*/
+
+        if(figure.javaClass.simpleName == "Square"){
+            thumbnailImageView.setImageResource(R.drawable.square)
+        }
+        else if(figure.javaClass.simpleName == "Triangle"){
+            thumbnailImageView.setImageResource(R.drawable.triangle)
+        }
+        else if(figure.javaClass.simpleName == "Circle"){
+            thumbnailImageView.setImageResource(R.drawable.checkbox_blank_circle)
+        }
+        else thumbnailImageView.setImageResource(R.drawable.ic_launcher_foreground)
+
+
+
+
+        return rowView
+    }
+
+}
--- Shapes/app/src/main/java/com/example/shapes/MainActivity.kt
+++ Shapes/app/src/main/java/com/example/shapes/MainActivity.kt
@@ -1,17 +1,87 @@
 package com.example.shapes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 
+
+
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var listView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
 
+        val listOfFigures: ArrayList<Figure> = randomFigures()
+
+
+
+        val listView: ListView = findViewById(R.id.listView)
+
+        //Adapter addList = new Adapter(this, listOfFigures)
+
+        val adapter = Adapter(this, listOfFigures)
+        listView.adapter = adapter
+
+        //adapter.addAll(listOfFigures)
+
+        //addList()
+    }
+
+    //var row: TableRow = findViewById<View>(android.R.id.display_row) as TableRow
+
+
+    fun addList() {
+
+    }
+
+    fun  randomFigures(): ArrayList<Figure> {
+        println("Hello world!")
+
+
+        val numberOfFigures = (1..10).random()  //ilość N figur do wylosowania
+
+        println("numberOfFigures $numberOfFigures")
+
+
+        val listOfFigures: MutableList<Figure> = mutableListOf()    //lista figur
+
+
+        for (i in 0 until numberOfFigures) {
+
+            when ((1..3).random()) {    //losowanie liczby 1 - 3 na podstawie której wybierana jest figura
+                1 -> {
+                    listOfFigures.add(Circle(Random.nextDouble(from = 0.0, until = 1.0)))   //dodanie Koła do listy o losowym promieniu z zakresu 0 - 1
+                }
+                2 -> {
+                    listOfFigures.add(Square(Random.nextDouble(from = 0.0, until = 1.0)))   //dodanie Kwadratu do listy o losowym boku z zakresu 0 - 1
+                }
+                3 -> {
+                    listOfFigures.add(Triangle(Random.nextDouble(from = 0.0, until = 1.0)))   //dodanie Trójkąta do listy o losowym boku z zakresu 0 - 1
+                }
+                else -> println("no such number")
+            }
+
+            //wyświetlenie każdego elementu listy figur
+            println(
+                "${listOfFigures[i].javaClass.simpleName} " +   //nazwa klasy
+                        "size: " + "%.3f".format(listOfFigures[i].size) +   //rozmiar promienia/boku z dokładnością do 3 miejsc po przecinku
+                        ", area: " + "%.3f".format(listOfFigures[i].figureArea) +   //pole figury z dokładnością do 3 miejsc po przecinku
+                        ", ${listOfFigures[i].characteristic}: " + "%.3f".format(listOfFigures[i].calculateCharacteristic()) //wartość charakteryzująca i jej wymiar z dokładnością do 3 miejsc po przecinku
+            )
+        }
+
+        return listOfFigures
+    }
+
+
+}
+/*
 fun main() {
 
     println("Hello world!")
@@ -49,4 +119,4 @@ fun main() {
         )
     }
 
-}
+}*/
