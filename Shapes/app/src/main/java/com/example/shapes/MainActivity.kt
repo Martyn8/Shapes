969142c44472c8c029b3daf0914aa7244dd29240
Martyna <martyn8mj@gmail.com>
2021-11-12 17:20:59 +0100

    feat: add menu and settings activity

--- Shapes/app/src/main/java/com/example/shapes/MainActivity.kt
+++ Shapes/app/src/main/java/com/example/shapes/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.shapes
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
@@ -11,20 +14,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val listOfFigures: ArrayList<Figure> = randomFigures()  //lista figur
 
         val listView: ListView = findViewById(R.id.listView)
 
 
-        val adapter = Adapter(this, listOfFigures)  //adapter dla ListView, otrzymuje listę figur do wyświetlenia
+        val adapter = Adapter(
+            this,
+            listOfFigures
+        )  //adapter dla ListView, otrzymuje listę figur do wyświetlenia
         listView.adapter = adapter
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item?.itemId) {
+            R.id.about -> {
+                val intent = Intent(this, About::class.java)
+                startActivity(intent)
+            }
+            R.id.settings -> {
+                val intent = Intent(this, Settings::class.java)
+                startActivity(intent)
+            }
+            R.id.stats -> {
+                val intent = Intent(this, Statistics::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     /*
     Funkcja odpowiedzialna za losowanie figur i ich wymiarów
      */
-    private fun  randomFigures(): ArrayList<Figure> {
+    private fun randomFigures(): ArrayList<Figure> {
         println("Hello world!")
 
 
@@ -40,13 +73,34 @@ class MainActivity : AppCompatActivity() {
 
             when ((1..3).random()) {    //losowanie liczby 1 - 3 na podstawie której wybierana jest figura
                 1 -> {
-                    listOfFigures.add(Circle(Random.nextDouble(from = 0.0, until = 1.0)))   //dodanie Koła do listy o losowym promieniu z zakresu 0 - 1
+                    listOfFigures.add(
+                        Circle(
+                            Random.nextDouble(
+                                from = 0.0,
+                                until = 1.0
+                            )
+                        )
+                    )   //dodanie Koła do listy o losowym promieniu z zakresu 0 - 1
                 }
                 2 -> {
-                    listOfFigures.add(Square(Random.nextDouble(from = 0.0, until = 1.0)))   //dodanie Kwadratu do listy o losowym boku z zakresu 0 - 1
+                    listOfFigures.add(
+                        Square(
+                            Random.nextDouble(
+                                from = 0.0,
+                                until = 1.0
+                            )
+                        )
+                    )   //dodanie Kwadratu do listy o losowym boku z zakresu 0 - 1
                 }
                 3 -> {
-                    listOfFigures.add(Triangle(Random.nextDouble(from = 0.0, until = 1.0)))   //dodanie Trójkąta do listy o losowym boku z zakresu 0 - 1
+                    listOfFigures.add(
+                        Triangle(
+                            Random.nextDouble(
+                                from = 0.0,
+                                until = 1.0
+                            )
+                        )
+                    )   //dodanie Trójkąta do listy o losowym boku z zakresu 0 - 1
                 }
                 else -> println("no such number")
             }
@@ -62,5 +116,5 @@ class MainActivity : AppCompatActivity() {
 
         return listOfFigures as ArrayList<Figure>
     }
-    
+
 }
